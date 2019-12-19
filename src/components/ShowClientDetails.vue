5cb6c947beaaa6af87269c467ffc112bffe3cf3f
jkyony <jkyony@gmail.com>
2019-12-19 12:20:21 +0200

    burgeon logo

--- src/components/ShowClientDetails.vue
+++ src/components/ShowClientDetails.vue
@@ -15,7 +15,7 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="profile-img">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt=""/>
+                            <img src="gs://burgean-care.appspot.com/burgeonEmailSignature.png" alt=""/>
                             <div class="file btn btn-lg btn-primary">
                                 Change Photo
                                 <input type="file" name="file"/>
